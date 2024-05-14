9b74495f805871aad5f884bcdceb2bfebcc43f85
Brooks Cunningham <bcunningham@fastly.com>
2024-05-14 08:55:08 -0500

    updates

--- edge-terraform-starter/dev-delivery/main.tf
+++ edge-terraform-starter/dev-delivery/main.tf
@@ -1,22 +1,23 @@
-# provider "sigsci" {
-#   # alias = "primary"
-#   corp       = var.NGWAF_CORP
-#   email      = var.NGWAF_EMAIL
-#   auth_token = var.NGWAF_TOKEN
-# }
+provider "sigsci" {
+  corp       = var.NGWAF_CORP
+  email      = var.NGWAF_EMAIL
+  auth_token = var.NGWAF_TOKEN
+}
+
+provider "fastly" {
+  api_key = var.FASTLY_API_KEY
+}
 
 module "service_vcl" {
   source                           = "../modules/service-vcl"
   FASTLY_API_KEY                   = var.FASTLY_API_KEY
   SERVICE_VCL_FRONTEND_DOMAIN_NAME = var.SERVICE_VCL_FRONTEND_DOMAIN_NAME
   SERVICE_VCL_BACKEND_HOSTNAME     = var.SERVICE_VCL_BACKEND_HOSTNAME
-  NGWAF_EMAIL                      = var.NGWAF_EMAIL
-  NGWAF_TOKEN                      = var.NGWAF_TOKEN
-  NGWAF_CORP                       = var.NGWAF_CORP
   NGWAF_SITE                       = var.NGWAF_SITE
-  # providers = {
-  #   sigsci = sigsci
-  # }
+  providers = {
+    sigsci = sigsci
+    fastly = fastly
+  }
 }
 
 output "live_waf_love" {
