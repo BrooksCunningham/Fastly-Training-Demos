8b3987198d87db4592d44393ce22d53b1c7d5bb5
Brooks Cunningham <Brooks.Cunningham@gmail.com>
2024-06-04 09:09:00 -0500

    Update providers.tf

--- edge-terraform-starter/dev-delivery/providers.tf
+++ edge-terraform-starter/dev-delivery/providers.tf
@@ -3,12 +3,10 @@ terraform {
     fastly = {
       source  = "fastly/fastly"
       version = ">= 5.7.0"
-    #   configuration_aliases = [ fastly ]
     }
     sigsci = {
       source  = "signalsciences/sigsci"
       version = ">= 2.1.0"
-    #   configuration_aliases = [ sigsci ]
     }
   }
 }
