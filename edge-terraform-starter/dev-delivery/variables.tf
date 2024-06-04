9d4e470e32a4e903e586871c5e8ff2d805cbd420
Brooks Cunningham <Brooks.Cunningham@gmail.com>
2024-06-04 09:08:40 -0500

    Update variables.tf

--- edge-terraform-starter/dev-delivery/variables.tf
+++ edge-terraform-starter/dev-delivery/variables.tf
@@ -18,20 +18,6 @@ variable "SERVICE_VCL_BACKEND_HOSTNAME" {
 
 #### VCL Service variables - End
 
-#### Compute Service variables - Start
-# variable "SERVICE_COMPUTE_FRONTEND_DOMAIN_NAME" {
-#   type = string
-#   description = "Frontend domain for your service."
-# }
-
-# variable "SERVICE_COMPUTE_BACKEND_HOSTNAME" {
-#   type          = string
-#   description   = "hostname used for backend."
-#   default       = "http-me.edgecompute.app"
-# }
-
-#### Compute Service variables - End
-
 #### NGWAF Vars - Start
 variable "NGWAF_CORP" {
     type        = string
